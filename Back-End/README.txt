edd56255d0aa824b4b893ff9a949ed29f666a441
Yosss <YosuaMontolaluXD@gmail.com>
2025-02-20 12:03:15 +0700

    Starting Point

---
+++ Back-End/README.txt
@@ -0,0 +1,1 @@
+Backend & AI taro disini
---
+++ Front-End/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="flex h-screen items-center justify-center text-8xl">
+        Hello, world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
